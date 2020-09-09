8caa52b82cc63a3e88801e57a638df1eca8578f5
William Silversmith <william.silversmith@gmail.com>
2020-09-09 17:51:20 -0400

    Appveyor test

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ from packaging.version import LegacyVersion
 from skbuild.exceptions import SKBuildError
 from skbuild.cmaker import get_cmake_version
 
+def read(fname):
+  with open(os.path.join(os.path.dirname(__file__), fname), 'rt') as f:
+    return f.read()
+
 # Add CMake as a build requirement if cmake is not installed or is too low a version
 setup_requires = []
 try:
@@ -54,6 +58,9 @@ setup(
     author = 'Manuel Castro',
     author_email = 'macastro@princeton.edu',
     url = 'https://github.com/seung-lab/DracoPy',
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    license = "License :: OSI Approved :: Apache Software License",
     cmake_source_dir='./draco',
     cmake_args=cmake_args,
     setup_requires=setup_requires,
@@ -68,5 +75,21 @@ setup(
             extra_compile_args=extra_compile_args,
             extra_link_args=extra_link_args
         )
-    ]
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Topic :: Utilities",
+  ]
 )
