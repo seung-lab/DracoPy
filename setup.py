655612fcc66522020717a51cc41444769368f170
Manuel Castro <manuel.a.castro1992@gmail.com>
2021-03-22 14:35:51 -0400

    v0.0.18

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ else:
 
 setup(
     name='DracoPy',
-    version='0.0.17',
+    version='0.0.18',
     description = 'Python wrapper for Google\'s Draco Mesh Compression Library',
     author = 'Manuel Castro',
     author_email = 'macastro@princeton.edu',
