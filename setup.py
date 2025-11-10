0e38388a2f0ab677f0cf4e1b14eb62efac90dd0a
William Silversmith <william.silversmith@gmail.com>
2025-11-10 13:45:04 -0500

    ci: update build system

--- setup.py
+++ setup.py
@@ -121,6 +121,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Scientific/Engineering",
         "Operating System :: POSIX",
         "Operating System :: MacOS",
